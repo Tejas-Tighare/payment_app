671990684236f99803ae9a74c8a9322ccae3e208
Tejas-Tighare <tejastighare18@gmail.com>
2025-12-28 15:05:17 +0530

    add cors and body parser

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,25 @@
 import express from "express";
 import connectDB from "./db.js";
-import { User } from "./models/user.js";
-import mainRouter from "./route/index.js"
+import mainRouter from "./route/index.js";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
-
- 
-
-connectDB();
 
 app.use("/api/v1", mainRouter);
 
+const startServer = async () => {
+  try {
+    await connectDB();  
+    app.listen(3000, () => {
+      console.log("Server running on port 3000");
+    });
+  } catch (error) {
+    console.error("Server failed to start", error);
+  }
+};
+
+startServer();
